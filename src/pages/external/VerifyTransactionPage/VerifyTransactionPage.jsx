6ad2b663f704454d19f095d3da76b4e574c30d0b
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-26 21:27:22 +0900

    feat(bank): 1원 송금자명을 확인하기 위한 외부 은행 어플 임시페이지 구현

---
+++ src/pages/external/VerifyTransactionPage/VerifyTransactionPage.jsx
@@ -0,0 +1,15 @@
+import Header from '../../../components/HeyMain/HeyHeader/HeyHeader';
+import UserCard from '../../../components/HeyMain/UserCard/UserCard';
+import MenuGrid from '../../../components/HeyMain/MenuGrid/MenuGrid';
+import BottomNavigation from '../../../components/BottomNavigation/BottomNavigation';
+
+const VerifyTransactionPage = () => {
+
+  return (
+    <div>
+      거래내역을 확인할 수 있는 페이지
+    </div>
+  );
+}
+
+export default VerifyTransactionPage;
