9264d8975e0b31b57fe8646c68d8b494498e93e7
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-28 01:41:57 +0900

    feat(api): 계좌 인증 관련 api 호출부 작성

--- src/util/accountAuthApi.js
+++ src/util/accountAuthApi.js
@@ -8,13 +8,28 @@ export const verifyAccount = async (email, accountId) => {
       email,
       accountId
     });
-    return response.data; // ApiResponse<Void> 반환
+    return response.data;
   } catch (error) {
     console.error('계좌 검증 실패:', error);
     throw error;
   }
 };
 
+export const confirmAccount = async (email, accountId, code) => {
+  try {
+    const response = await api.post(baseURL + '/api/accounts/verification/confirm', {
+      email,
+      accountId,
+      code
+    });
+    return response.data;
+  } catch (error) {
+    console.error('계좌 검증 실패:', error);
+    throw error;
+  }
+}
+
 export default {
   verifyAccount,
+  confirmAccount,
 };
