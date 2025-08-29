da7a140a247ead59b1dee036953898c6e8b663de
kwonssshyeon <kwonsh4396@gmail.com>
2025-08-30 08:20:23 +0900

    feat(payment): 결제 스켈레톤 페이지 구현 및 라우터 적용

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,6 +9,7 @@ import Solsolhanhankki from '../pages/Solsolhanhankki/Solsolhanhankki';
 import DdangyoMock from '../pages/DdangyoMock';
 import RestaurantDetail from '../pages/RestaurantDetail/RestaurantDetail';
 import MenuDetail from '../pages/MenuDetail/MenuDetail';
+import PaymentPage from '../pages/heyyoung/Payment/PaymentPage';
 
 const AppRoutes = () => {
   return (
@@ -19,6 +20,7 @@ const AppRoutes = () => {
       <Route path="/account-manage" element={<AccountManagePage />} />
       <Route path="/account-connect" element={<AccountConnectPage />} />
       <Route path='/verify-transaction' element={<VerifyTransactionPage />} />
+      <Route path="/payment" element={<PaymentPage />} /> 
       <Route path="/ddangyo-mock" element={<DdangyoMock />} />
       <Route path="/restaurants/:id" element={<RestaurantDetail />} />
       <Route path="/menus/:menuId" element={<MenuDetail />} />
